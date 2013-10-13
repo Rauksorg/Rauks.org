2cb57cbae1b296e353a4d73f4c726b4fd113bd3c
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2013-10-13 11:10:29 -0400

    google webmaster

--- public/JS/rauks.js
+++ public/JS/rauks.js
@@ -16,19 +16,6 @@
 // along with Rauks.org. If not, see <http://www.gnu.org/licenses/>.
 
 $(document).on("pageinit", function() {
-    (function(i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r;
-        i[r] = i[r] || function() {
-            (i[r].q = i[r].q || []).push(arguments)
-        }, i[r].l = 1 * new Date();
-        a = s.createElement(o),
-        m = s.getElementsByTagName(o)[0];
-        a.async = 1;
-        a.src = g;
-        m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-    ga('create', 'UA-28391166-1', 'rauks.org');
-    ga('send', 'pageview');
 });
 
