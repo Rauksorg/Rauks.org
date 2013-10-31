e9592b43be1d3b349d916343587d7a2cb609efe7
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2013-10-31 16:46:19 -0400

    jade templates

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-// Copyright 2013 Thibaut CONSTANT
-// This file is part of Rauks.org.
-// 
-//     Rauks.org is free software: you can redistribute it and/or modify
-//     it under the terms of the GNU General Public License as published by
-//     the Free Software Foundation, either version 3 of the License, or
-//     (at your option) any later version.
-// 
-//     Rauks.org is distributed in the hope that it will be useful,
-//     but WITHOUT ANY WARRANTY; without even the implied warranty of
-//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//     GNU General Public License for more details.
-// 
-//     You should have received a copy of the GNU General Public License
-//     along with Rauks.org.  If not, see <http://www.gnu.org/licenses/>.
-// 
+/*
+Copyright 2013 Thibaut CONSTANT
+This file is part of Rauks.org.
+ 
+Rauks.org is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+ 
+Rauks.org is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with Rauks.org.  If not, see <http://www.gnu.org/licenses/>.
+*/
 // "use strict";
 var directory = __dirname + '/public';
 var express = require("express"),
@@ -26,7 +27,7 @@ server.listen(process.env.PORT, process.env.IP);
 // 
 // Routeur :
 app.get('/', function(req, res) {
-    res.sendfile( directory + "/home.html");
+    res.render("home.jade",{"title":"Rauks.org jeu de rôles"});
 });
 app.get('/about', function(req, res) {
     res.sendfile(directory + "/about.html");
