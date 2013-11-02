dc5ae7f55df2d2f76d5e7efcbea8816990c91c5a
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2013-11-01 22:35:43 -0400

    cleanup

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ You should have received a copy of the GNU General Public License
 along with Rauks.org.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-var directory = __dirname + '/public';
 var express = require("express"),
     app = express();
 app.configure(function() {
@@ -30,7 +29,7 @@ mongoose.connect("mongodb://test:test@dharma.mongohq.com:10009/app17028231");
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongodb connection error:'));
 db.once('open', function callback() {
-    
+
     //Mongoose schema
     var systemSchema = mongoose.Schema({
         name: String,
@@ -38,48 +37,50 @@ db.once('open', function callback() {
         title: String,
         text: String
     });
-    
+
     // Mongoose model
     var system = mongoose.model('system', systemSchema);
 
-    // Routeur :
-    app.get('/', function(req, res) {
-        res.render("home.jade", {
-            "pagetitle": "Rauks.org jeu de rôles Electropunk",
-            "title": "Rauks.org jeu de rôles Electropunk"
-        });
-    });
-    app.get('/about', function(req, res) {
-        res.render("about.jade", {
-            "pagetitle": "Autour du jeu de rôles Rauks.org",
-            "title": "Autour du jeu"
-        });
-    });
-    app.get('/system_index', function(req, res) {
-        res.render("system_index.jade", {
-            "pagetitle": "Système du jeu de rôles Rauks.org",
-            "title": "Système de jeu"
-        });
-    });
-    
-    // system routeur
+    // db driven routeur
     app.get('/system', function(req, res) {
         var system_name = req.query.n;
-        system.findOne({
-            name: system_name
+        console.log(system_name);
+        if (system_name === undefined) {
+            res.render("system_list.jade", {
+                "pagetitle": "Système du jeu de rôles Rauks.org",
+                "title": "Système de jeu"
+            });
+        }
+        else {
+            system.findOne({
+                name: system_name
 
-        }, function(err, foundname) {
-            if (err) console.log("name query error");
-            if (foundname === null) {
-                res.send(404, 'Sorry cant find that!');
-            }
-            else {
-                res.render("system_template.jade", foundname);
-            }
-        });
+            }, function(err, foundname) {
+                if (err) console.log("name query error");
+                if (foundname === null) {
+                    res.send(404, 'Sorry cant find that!');
+                }
+                else {
+                    res.render("system_template.jade", foundname);
+                }
+            });
+        }
     });
-    //Handle 404
-    app.use(function(req, res, next) {
-        res.send(404, 'Sorry cant find that!');
+});
+// Routeur :
+app.get('/', function(req, res) {
+    res.render("home.jade", {
+        "pagetitle": "Rauks.org jeu de rôles Electropunk",
+        "title": "Rauks.org jeu de rôles Electropunk"
     });
+});
+app.get('/about', function(req, res) {
+    res.render("about.jade", {
+        "pagetitle": "Autour du jeu de rôles Rauks.org",
+        "title": "Autour du jeu"
+    });
+});
+//Handle 404
+app.use(function(req, res, next) {
+    res.send(404, 'Sorry cant find that!');
 });
