f54d0ff440448b6b35af7395322433c580474a44
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2013-11-02 20:10:35 -0400

    jqm_wysihtml

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ The source code of the table role playing game rauks.org, www.rauks.org.
 
 Based on nodejs, it uses mongodb through mongoose for database driven pages.
 
-It has also a wysihtml5 adaptation to jquery mobile.
+It has also a basic wysihtml5 adaptation to jquery mobile (lookink for help to improve it).
---
+++ public/js/jqm_wysihtml5.js
@@ -0,0 +1,13 @@
+$(document).on("pageinit", function() {
+
+
+    var editor = new wysihtml5.Editor("wysihtml5-textarea", { // id of textarea element
+        toolbar: "wysihtml5-toolbar", // id of toolbar element
+        parserRules: wysihtml5ParserRules, // defined in parser rules set
+        stylesheets: ["/css/editor.css"]
+    });
+    editor.on('load', function() {
+        // The wysiwyg editor is in the DOM. It's safe to make the plugin call
+        $(editor.composer.iframe).wysihtml5_size_matters();
+    });
+});
