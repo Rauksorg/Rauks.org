85be9f9060170793abc52ade6f893be2b7ad7ded
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2013-11-08 13:22:31 -0500

    private system

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with Rauks.org.  If not, see <http://www.gnu.org/licenses/>.
 */
 if (process.env.C9_PROJECT) {
-    require('./db_url.js');
+    require('./private.js');
 }
 var express = require("express"),
     app = express();
