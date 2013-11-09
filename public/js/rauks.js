4ce9c4e3cbce6d37498c4ecf45920b9024ce4bc3
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2013-11-09 14:45:12 -0500

    goggle analytics

--- public/js/rauks.js
+++ public/js/rauks.js
@@ -1,37 +1,33 @@
 /*
 Copyright 2013 Thibaut CONSTANT
 This file is part of Rauks.org.
- 
+
 Rauks.org is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
- 
+
 Rauks.org is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
- 
+
 You should have received a copy of the GNU General Public License
 along with Rauks.org.  If not, see <http://www.gnu.org/licenses/>.
 */
-
-$(document).on("pageinit", function() {
+$(document).on("pageinit", function () {
     tinymce.init({
-    selector: "h1.edit",
-    inline: true,
-    toolbar: "undo redo",
-    menubar: false
-});
+        selector: "h1.edit",
+        inline: true,
+        toolbar: "undo redo",
+        menubar: false
+    });
     tinymce.init({
         selector: "div.edit",
         inline: true,
-        plugins: [
-        "advlist autolink lists link image charmap print preview hr anchor pagebreak",
-        "searchreplace wordcount visualblocks visualchars code fullscreen",
-        "insertdatetime media nonbreaking save table contextmenu directionality"
-    ],
-    toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media",
-    image_advtab: true,
+        plugins: "advlist autolink lists link image charmap print preview hr anchor pagebreak searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking save table contextmenu directionality charmap",
+        toolbar1: "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist outdent indent | table link image charmap media | code",
+        fixed_toolbar_container: "#header",
+        menubar: false
     });
 });
