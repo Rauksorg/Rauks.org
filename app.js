7481e5c5b161a8b6e811d0ef0cc9ab797bedce25
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2013-11-10 21:28:40 -0500

    fixing

--- app.js
+++ app.js
@@ -58,7 +58,38 @@ var article = mongoose.model('article', articleSchema);
 db.once('open', function callback() {
     // 
     // db driven routeur function
-   var db_driven_route = require('./db_driven_route.js');
+   var db_driven_route = function (route, pagetitle, title) {
+    app.get('/' + route, function (req, res) {
+        var system_name = req.query.n;
+        if (system_name === undefined) {
+            article.find({
+                category: route
+            }, {
+                title: 1,
+                name: 1,
+                _id: 0
+            }, function (err, foundarticle) {
+                res.render(route + ".jade", {
+                    "foundarticle": foundarticle,
+                    "pagetitle": pagetitle,
+                    "title": title
+                });
+            });
+        } else {
+            article.findOne({
+                name: system_name
+            }, function (err, foundname) {
+                if (err) console.log("name query error");
+                if (foundname === null) {
+                    res.send(404, 'Sorry cant find that!');
+                } else {
+                    // foundname render page title, title and text
+                    res.render(route + "_page.jade", foundname);
+                }
+            });
+        }
+    });
+};
     // 
     // declare routes
     db_driven_route("about","Autour du jeu de rôles Rauks.org","Pourquoi nous avons développé Rauks.org ?");
