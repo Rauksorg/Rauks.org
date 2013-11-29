7b947a6ac6f070022fa3a0e868711fd3b22917c6
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2013-11-28 19:32:25 -0500

    gzip

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ var express = require("express"),
     app = express(),
     oneYear = 31557600000;
 app.configure(function() {
+    app.use(express.compress());
     app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
     app.use(express.bodyParser());
 });
